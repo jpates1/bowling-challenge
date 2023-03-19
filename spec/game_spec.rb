4e62d30414d613c12e3dad5539b5455e47de17aa
jpates1 <jamesandrewpates@gmail.com>
2023-03-19 18:31:41 +0000

    first test - rolls

---
+++ spec/game_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe BowlingGame do
+  describe "#roll" do
+  it "adds the number of pins knocked down to the rolls array" do
+  game = BowlingGame.new
+  game.roll(5)
+  expect(game.instance_variable_get(:@rolls)).to eq([5])
+  end
+  end
+  end
