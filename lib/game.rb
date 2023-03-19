93a40c6fb2311d6f77a19c918fcbc999c854dde0
jpates1 <jamesandrewpates@gmail.com>
2023-03-19 19:14:47 +0000

    tests for calc a perfect game, pass

--- lib/game.rb
+++ lib/game.rb
@@ -1,27 +1,39 @@
 class BowlingGame
   def initialize
-  @rolls = []
+    @rolls = []
   end
-  
+
   def roll(pins)
-  @rolls << pins
+    @rolls << pins
   end
-  
+
   def score
-  total_score = 0
-  roll_index = 0
-  10.times do
-  if @rolls[roll_index] == 10 # strike
-  total_score += 10 + @rolls[roll_index + 1] + @rolls[roll_index + 2]
-  roll_index += 1
-  elsif @rolls[roll_index] + @rolls[roll_index + 1] == 10 # spare
-  total_score += 10 + @rolls[roll_index + 2]
-  roll_index += 2
-  else
-  total_score += @rolls[roll_index] + @rolls[roll_index + 1]
-  roll_index += 2
+    total_score = 0
+    roll_index = 0
+
+    10.times do
+      if strike?(roll_index)
+        total_score += 10 + @rolls[roll_index+1] + @rolls[roll_index+2]
+        roll_index += 1
+      elsif spare?(roll_index)
+        total_score += 10 + @rolls[roll_index+2]
+        roll_index += 2
+      else
+        total_score += @rolls[roll_index] + @rolls[roll_index+1]
+        roll_index += 2
+      end
+    end
+
+    total_score
   end
+
+  private
+
+  def strike?(roll_index)
+    @rolls[roll_index] == 10
   end
-  total_score
+
+  def spare?(roll_index)
+    @rolls[roll_index] + @rolls[roll_index+1] == 10
   end
-  end
+end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -21,14 +21,32 @@ RSpec.describe BowlingGame do
       20.times { game.roll(4) }
       expect(game.score).to eq(80)
     end
+
+
+    it "correctly calculates a spare" do
+      game = BowlingGame.new
+      game.roll(5)
+      game.roll(5) # spare
+      game.roll(3)
+      17.times { game.roll(0) }
+      expect(game.score).to eq(16)
+    end
+
+    it "correctly calculates a strike" do
+      game = BowlingGame.new
+      game.roll(10) # strike
+      game.roll(3)
+      game.roll(4)
+      16.times { game.roll(0) }
+      expect(game.score).to eq(24)
+    end
+
+    it "correctly calculates a perfect game" do
+      game = BowlingGame.new
+      12.times { game.roll(10) }
+      expect(game.score).to eq(300)
+    end
   end
 
-  it "correctly calculates a spare" do
-    game = BowlingGame.new
-    game.roll(5)
-    game.roll(5) # spare
-    game.roll(3)
-    17.times { game.roll(0) }
-    expect(game.score).to eq(16)
-  end
+
 end
