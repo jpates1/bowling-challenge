e9a1fecb0c6adb73cb5c16bf12059ed6aecada7a
jpates1 <jamesandrewpates@gmail.com>
2023-03-19 19:07:53 +0000

    tests for calc a spare, pass

--- lib/game.rb
+++ lib/game.rb
@@ -1,17 +1,27 @@
 class BowlingGame
   def initialize
-    @rolls = []
+  @rolls = []
   end
   
   def roll(pins)
-    @rolls << pins
+  @rolls << pins
   end
   
   def score
-    @rolls.sum
+  total_score = 0
+  roll_index = 0
+  10.times do
+  if @rolls[roll_index] == 10 # strike
+  total_score += 10 + @rolls[roll_index + 1] + @rolls[roll_index + 2]
+  roll_index += 1
+  elsif @rolls[roll_index] + @rolls[roll_index + 1] == 10 # spare
+  total_score += 10 + @rolls[roll_index + 2]
+  roll_index += 2
+  else
+  total_score += @rolls[roll_index] + @rolls[roll_index + 1]
+  roll_index += 2
   end
-  
-  def frame_score(frame_index)
-    @rolls[frame_index] + @rolls[frame_index + 1]
   end
-end
+  total_score
+  end
+  end
