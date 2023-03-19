452b2354ae6409405a49ae7311a504d3fcf6f3d0
jpates1 <jamesandrewpates@gmail.com>
2023-03-19 18:33:52 +0000

    second test - score

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,11 +1,17 @@
-require 'game'
-
 RSpec.describe BowlingGame do
   describe "#roll" do
   it "adds the number of pins knocked down to the rolls array" do
   game = BowlingGame.new
   game.roll(5)
   expect(game.instance_variable_get(:@rolls)).to eq([5])
+  end
+  end
+  
+  describe "#score" do
+  it "returns 0 for a gutter game" do
+  game = BowlingGame.new
+  20.times { game.roll(0) }
+  expect(game.score).to eq(0)
   end
   end
   end
